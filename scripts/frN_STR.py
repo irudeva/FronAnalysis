268ffa5d805a3332fb7dd8445993f3edb71b6a52
irudeva <irinarudeva@gmail.com>
2017-05-26 06:33:59 +1000

    f

--- scripts/frN_STR.py
+++ scripts/frN_STR.py
@@ -220,7 +220,6 @@ for yr in yrs:
                                       fr_northlat[yr-year[0],nt-tfr1,ifr] = np.amax(flat[nt,ifr,:npts[nt,ifr]])
                                     #   print yr-year[0],nt-tfr1,ifr, "   ",im,month_abbr[im], frmask[yr-year[0],nt-tfr1,ifr]
                                     #   print np.sum(frmask==im)
-                                      print fr_northlat[yr-year[0],nt-tfr1,ifr]
                                       break
 
 
@@ -250,7 +249,7 @@ for im in range( 1,13):
 for yr in yrs:
     for im in range( 1,13):
         nfr_my[0,im-1,yr-year[0]]=np.sum(frmask[yr-year[0],:,:]==im)
-        fr_northlat_my[0,im-1,yr-year[0]]=np.mean(fr_northlat_my[frmask[yr-year[0],:,:]==im])
+        fr_northlat_my[0,im-1,yr-year[0]]=np.mean(fr_northlat_my[yr-year[0],:,:][np.where(frmask[yr-year[0],:,:]==im)])
         tmp  = frdv[yr-year[0],:,:][np.where(frmask[yr-year[0],:,:] == im)]
         nfr_my3tr[0,im-1,yr-year[0]]=np.count_nonzero(np.where(tmp>=dvcrit[im-1], 1,0))
         tmp1 = fr_northlat[yr-year[0],:,:][np.where(frmask[yr-year[0],:,:] == im)]
