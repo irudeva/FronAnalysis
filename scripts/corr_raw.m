da57fc774ccd86073de67195dffb836a91da5382
irudeva <irinarudeva@gmail.com>
2017-07-27 18:17:47 +1000

    matlab

--- scripts/corr_raw.m
+++ scripts/corr_raw.m
@@ -5,27 +5,35 @@
 sec = { '0_90S','30_90E.0_90S','90_150E.0_90S','150_210E.0_90S','210_285E.0_90S','300_340E.0_90S'};
 ssn = { 'YYY','DJF','MAM','JJA','SON'};
 
-cc_STRloc_frN = double.empty(5,0);
-cc_STRint_frN = double.empty(5,0);
-for i = 1:5
+yr1= 1979;
+yr2 = 2015;
+yrs = (yr1:yr2);
+nyrs = length(yrs);
+
+
+cc_STRloc_frN = zeros(6,5);
+cc_STRint_frN = zeros(6,5);
+for isec = 1:6
+for issn = 1:5
     
-    if i ~=2 
-        pc_STRloc_frN_mslp65(i) = partialcorri(STRloc(:,i),frN(:,i),mslp65(:,i));
-        cc = corrcoef(STRloc(:,i),frN(:,i));
-        cc_STRloc_frN(i) = cc(1,2);
-        pc_STRint_frN_mslp65(i) = partialcorri(STRint(:,i),frN(:,i),mslp65(:,i));
-        cc = corrcoef(STRint(:,i),frN(:,i));
-        cc_STRint_frN(i) = cc(1,2);
+    if issn ~=2 
+        pc_STRloc_frN_mslp65(isec,issn) = partialcorri(squeeze(STRloc(isec,issn,:)),squeeze(frN(isec,issn,:)),squeeze(mslp65(isec,issn,:)));
+        cc = corrcoef(STRloc(isec,issn,:),frN(isec,issn,:));
+        cc_STRloc_frN(isec,issn) = cc(1,2);
+        pc_STRint_frN_mslp65(isec,issn) = partialcorri(squeeze(STRint(isec,issn,:)),squeeze(frN(isec,issn,:)),squeeze(mslp65(isec,issn,:)));
+        cc = corrcoef(STRint(isec,issn,:),frN(isec,issn,:));
+        cc_STRint_frN(isec,issn) = cc(1,2);
    else
-        nyrs = size(yrs(:,1));
-        pc_STRloc_frN_mslp65(i) = partialcorri(STRloc(2:nyrs,i),frN(2:nyrs,i),mslp65(2:nyrs,i));
-        cc = corrcoef(STRloc(2:nyrs,i),frN(2:nyrs,i));
-        cc_STRloc_frN(i) = cc(1,2);
-        pc_STRint_frN_mslp65(i) = partialcorri(STRint(2:nyrs,i),frN(2:nyrs,i),mslp65(2:nyrs,i));
-        cc = corrcoef(STRint(2:nyrs,i),frN(2:nyrs,i));
-        cc_STRint_frN(i) = cc(1,2);
+        %nyrs = size(yrs(:,1));
+        pc_STRloc_frN_mslp65(isec,issn) = partialcorri(squeeze(STRloc(isec,issn,2:nyrs)),squeeze(frN(isec,issn,2:nyrs)),squeeze(mslp65(isec,issn,2:nyrs)));
+        cc = corrcoef(STRloc(isec,issn,2:nyrs),frN(isec,issn,2:nyrs));
+        cc_STRloc_frN(isec,issn) = cc(1,2);
+        pc_STRint_frN_mslp65(isec,issn) = partialcorri(squeeze(STRint(isec,issn,2:nyrs)),squeeze(frN(isec,issn,2:nyrs)),squeeze(mslp65(isec,issn,2:nyrs)));
+        cc = corrcoef(STRint(isec,issn,2:nyrs),frN(isec,issn,2:nyrs));
+        cc_STRint_frN(isec,issn) = cc(1,2);
    end
     
+end
 end
 
 figure
---
+++ scripts/ts_import.m
@@ -0,0 +1,490 @@
+%% Import data from text file.
+
+sec = { '0_90S','30_90E.0_90S','90_150E.0_90S','150_210E.0_90S','210_285E.0_90S','300_20E.0_90S'};
+fsec = { '20_40S','30_90E.20_40S','90_150E.20_40S','150_210E.20_40S','210_285E.20_40S','300_20E.20_40S'};
+nsec = length(sec);
+yr1= 1979;
+yr2 = 2015;
+yrs = (yr1:yr2);
+nyrs = length(yrs);
+
+
+% read frN
+frN = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/frN.',fsec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*8s%9s%9s%9s%9s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ frN(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
+
+% read STRint
+STRint = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/STRint.',sec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*7s%8s%8s%8s%8s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ STRint(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
+
+% read STRloc
+STRloc = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/STRloc.',sec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*7s%8s%8s%8s%8s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ STRloc(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
+
+% read mslp40
+mslp40 = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/mslp40.',sec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*7s%8s%8s%8s%8s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ mslp40(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
+
+% read mslp65
+mslp65 = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/mslp65.',sec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*7s%8s%8s%8s%8s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ mslp65(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
+
+% read SAMssn
+SAMssn = zeros(nsec,5,nyrs); 
+
+for isec = 1:6
+
+%% Initialize variables
+filename = strcat('/Users/irudeva/work/Projects/Front/FrontAnalysis/output/ts/SAMssn.',sec(isec),'.1979_2015.txt');
+fn = char(filename);
+startRow = 2;
+
+%% Read columns of data as strings:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%*7s%8s%8s%8s%8s%s%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'WhiteSpace', '', 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Convert the contents of columns containing numeric strings to numbers.
+% Replace non-numeric strings with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+numericData = NaN(size(dataArray{1},1),size(dataArray,2));
+
+for col=[1,2,3,4,5]
+    % Converts strings in the input cell array to numbers. Replaced non-numeric
+    % strings with NaN.
+    rawData = dataArray{col};
+    for row=1:size(rawData, 1);
+        % Create a regular expression to detect and remove non-numeric prefixes and
+        % suffixes.
+        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
+        try
+            result = regexp(rawData{row}, regexstr, 'names');
+            numbers = result.numbers;
+            
+            % Detected commas in non-thousand locations.
+            invalidThousandsSeparator = false;
+            if any(numbers==',');
+                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
+                if isempty(regexp(numbers, thousandsRegExp, 'once'));
+                    numbers = NaN;
+                    invalidThousandsSeparator = true;
+                end
+            end
+            % Convert numeric strings to numbers.
+            if ~invalidThousandsSeparator;
+                numbers = textscan(strrep(numbers, ',', ''), '%f');
+                numericData(row, col) = numbers{1};
+                raw{row, col} = numbers{1};
+            end
+        catch me
+        end
+    end
+end
+
+
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
+%% Allocate imported array to column variable names
+for issn = 1:5
+ SAMssn(isec,issn,:) = cell2mat(raw(:,issn));
+end
+
+%% Clear temporary variables
+clearvars fn filename startRow formatSpec fileID dataArray ans raw col numericData rawData row regexstr result numbers invalidThousandsSeparator thousandsRegExp me R;
+end
